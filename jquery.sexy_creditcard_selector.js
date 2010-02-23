cb43d1621dc82255e7ba7fdfc340489ac812498d
Amiel Martin <amiel@carnesmedia.com>
2010-02-23 11:54:15 -0800

    better comment headers and include minified version

--- jquery.sexy_creditcard_selector.js
+++ jquery.sexy_creditcard_selector.js
@@ -1,3 +1,11 @@
+/*
+ * http://github.com/amiel/sexy_creditcard_selector
+ * Amiel Martin
+ * 2010-01-19
+ * 
+ * When a valid credit card number is input to a text field, this jQuery plugin calls a callback with the credit card company name.
+ */
+
 (function($) {
 	// generated with:  puts ActiveMerchant::Billing::CreditCardMethods::CARD_COMPANIES.to_json
 	var credit_company_regexes = {"switch": /^6759\d{12}(\d{2,3})?$/, "visa": /^4\d{12}(\d{3})?$/, "diners_club": /^3(0[0-5]|[68]\d)\d{11}$/, "master": /^(5[1-5]\d{4}|677189)\d{10}$/, "forbrugsforeningen": /^600722\d{10}$/, "dankort": /^5019\d{12}$/, "laser": /^(6304[89]\d{11}(\d{2,3})?|670695\d{13})$/, "american_express": /^3[47]\d{13}$/, "solo": /^6767\d{12}(\d{2,3})?$/, "jcb": /^3528\d{12}$/, "discover": /^(6011|65\d{2})\d{12}$/, "maestro": /^(5[06-8]|6\d)\d{10,17}$/};
